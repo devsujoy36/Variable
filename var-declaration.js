3122728f358c365adf735fd415e7ed16e8d05c1b
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-21 15:42:48 +0600

    first commit

---
+++ var-declaration.js
@@ -0,0 +1,12 @@
+var a = 465; //normal declaration
+var b = [45, 45, 78, 64, 52, 28]; //array declaration
+var c = { Name: 'Sujoy Das', Roll: 672736, Semester: '5th', Shift: '2nd', Brilliant: 'yes' } //Object declaration
+
+console.log('a = ', a);
+console.log('typeof a = ',typeof a);
+
+console.log('b = ', b);
+console.log('typeof b = ',typeof b);
+
+console.log('c = ', c);
+console.log('typeof c = ',typeof c);
---
+++ variable.js
@@ -0,0 +1,8 @@
+// Variable er namer age 'var' use kore variable er man change kora jay
+var a = 55;
+
+// Variable er namer age 'let' use kore variable er man change kora jay
+let b = 54;
+
+// Variable er namer age 'const' use kore variable er man change kora jay na
+const c = 56;
